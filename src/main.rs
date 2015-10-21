a62142a7ce0c9d6850c45a4d57a766aea38e6f0d
Pierre Baillet <pierre@baillet.name>
2015-10-21 10:11:38 +0200

    Files.

---
+++ src/main.rs
@@ -0,0 +1,34 @@
+extern crate notify;
+
+fn watch(file: &str) -> notify::FsEventWatcher {
+    use notify::Watcher;
+    use std::sync::mpsc::channel;
+    use std::thread;
+
+    // Create a channel to receive the events.
+    let (tx, rx) = channel();
+
+    let mut watcher = notify::new(tx).unwrap();
+    watcher.watch(file).unwrap();
+
+    // Don't join this thread
+    thread::spawn(move || {
+        loop {
+        match rx.recv() {
+            _ => println!("changes!")
+        }
+        }
+    });
+
+    watcher
+}
+
+fn main() {
+    // Don't let the watcher drop
+    let _watcher = watch("./test.ares");
+    // in my real application, this sleep-loop
+    // is actually a webserver that is running
+    loop {
+        ::std::thread::sleep_ms(20);
+    }
+}
